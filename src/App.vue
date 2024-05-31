4a23f639fa7c976603a08a0833cd92ce71409a8f
marroccuandrea <marroccuandrea@yahoo.it>
2024-05-31 17:12:09 +0200

    Milestone 3 e 4 fatta

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script>
+import axios from 'axios';
+import {store} from './data/store'
+
+export default {
+    data(){
+      return {
+
+      }
+    },
+
+    methods:{
+      getApi(){
+        axios.get(store.apiUrl)
+        .then(result => {
+            console.log(result.data);
+        })
+        .catch(error => {
+          console.log(error.message);
+        })
+      }
+
+    },
+
+    mounted(){
+      this.getApi();
+
+    },
+  }
+</script>
+
+<template>
+  <div>
+prova
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
